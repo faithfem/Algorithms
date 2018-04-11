e07952def87bc1c55d8daf7f5236a31d1bb6f986
Faith <fmugure@intracitygeeks.org>
2018-04-11 23:59:48 +0000

    ransomNote

--- ransomNote.js
+++ ransomNote.js
@@ -3,12 +3,21 @@ function ransomNote(noteText, magazineText)
     var noteArr = noteText.split('');
     var magazineArr = magazineText.split('');
     var magazineObj = {};
-    
-    magazineArr.forEach(word => 
+    var noteObj= {};
+
+   magazineArr.forEach(word =>
     {
         if(!magazineObj[word]) magazineObj[word] = 0;
         magazineObj[word]++;
     });
     console.log(magazineObj);
+
+    noteArr.forEach(word =>
+    {
+        if(!noteObj[word]) noteObj[word] = 0;
+        noteObj[word]++;
+    });
+    console.log(noteObj);
 }
-ransomNote(" ","this is all the magazine text in the magazine");
+ransomNote("","this is all text");
+ransomNote("","we have your grandmorther!");
