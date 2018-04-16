f35446f29bbc89009e7702eb5bfb7aa11c238a53
Faith <fmugure@intracitygeeks.org>
2018-04-16 02:53:54 +0000

    CaesarCipher

--- CaesarCipher.js
+++ CaesarCipher.js
@@ -1,4 +1,15 @@
-function CaesarCipher(string)
+function CaesarCipher(str, num)
 {
+    var lowerCaseString = str.LowerCase();
+    var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+    
+    for (var i = 0; i<lowerCaseString.length; i++){
+        var currentLetter = lowerCaseString[i];
+        if(currentLetter ===""){
+            newString +=currentLetter;
+            continue;
+        }
+        
+    }
     
 }
