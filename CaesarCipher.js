81245cf02869df61ece1d7a63edb77472d672713
Faith <fmugure@intracitygeeks.org>
2018-04-17 23:44:52 +0000

    CaesarCipher

--- CaesarCipher.js
+++ CaesarCipher.js
@@ -1,7 +1,33 @@
-function CaesarCipher(str, num)
-{
 //Takes in 2 parameters; a string and a number
 //The string is moved the number of spots dictated by the number
 //eg if the parameters are (bark, 1) then b ecomes c, a becomes b, r is s, k is l
+//If you get to end of alphabet, code has to be able to loop around
+//Code has to be able to accept negative numbers so we can shift back
 
+function CaesarCipher(str, num)
+{
+num = num %26; //takes care of numbers greater/lower than +-26
+//first make entire string into lowercase
+var lowerCaseString = str.toLowerCase();
+var alphabet = "abcdefghijklmnopqrstuvwxyz".split();
+var newString = " ";
+
+for(var i = 0; i<lowerCaseString.length; i++){
+    var currentLetter = lowerCaseString[i];
+    if(currentLetter === " "){
+        newString += currentLetter;
+        continue;
+    }
+    var currentIndex = alphabet.indexOf(currentLetter);
+    var newIndex = currentIndex + num;
+    if(newIndex > 25) newIndex = newIndex - 26; //loop code
+    if(newIndex < 0) newIndex = newIndex + 26;
+    
+    if(str[i]===str[i].toUpperCase()){
+        newString+= alphabet[newIndex].toUpperCase();
+    }
+    
+    else newString +=alphabet[newIndex];
+}
+    return newString;
 }
