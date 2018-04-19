f511d41b95b7f6974955eb31934f5c2d862f05ff
Faith <fmugure@intracitygeeks.org>
2018-04-19 11:06:27 +0000

    CaesarCipher

--- CaesarCipher.js
+++ CaesarCipher.js
@@ -9,12 +9,12 @@ function CaesarCipher(str, num)
 num = num %26; //takes care of numbers greater/lower than +-26
 //first make entire string into lowercase
 var lowerCaseString = str.toLowerCase();
-var alphabet = "abcdefghijklmnopqrstuvwxyz".split();
-var newString = " ";
+var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split();
+var newString = '';
 
-for(var i = 0; i<lowerCaseString.length; i++){
+for(var i = 0; i < lowerCaseString.length; i++){
     var currentLetter = lowerCaseString[i];
-    if(currentLetter === " "){
+    if(currentLetter === ''){
         newString += currentLetter;
         continue;
     }
@@ -23,13 +23,13 @@ for(var i = 0; i<lowerCaseString.length; i++){
     if(newIndex > 25) newIndex = newIndex - 26; //loop code
     if(newIndex < 0) newIndex = newIndex + 26;
     
-    if(str[i]===str[i].toUpperCase()){
-        newString+= alphabet[newIndex].toUpperCase();
+    /*if(str[i] === str[i].toUpperCase()){
+        newString += alphabet[newIndex].toUpperCase();
     }
     
-    else newString +=alphabet[newIndex];
+    else newString += alphabet[newIndex];*/
 }
     return newString;
 }
 
-CaesarCipher("Zoo Keeper", 2);
+CaesarCipher('zookeeper', 2);
